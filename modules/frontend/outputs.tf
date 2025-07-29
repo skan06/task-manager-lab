a3325ea7c08aafac2641cf18991e15391652a91e
skan06 <sk06.hd@gmail.com>
2025-07-29 16:53:16 +0100

    Add s3_website_endpoint output and verify all Terraform files

--- modules/frontend/outputs.tf
+++ modules/frontend/outputs.tf
@@ -1,5 +1,5 @@
-# Output the S3 website URL
-output "s3_website_url" {
-  description = "Frontend website URL"                    # Description of the output
-  value       = aws_s3_bucket_website_configuration.frontend_config.website_endpoint # S3 website endpoint
+# Output the S3 website endpoint for use in CORS configuration in the backend module
+output "s3_website_endpoint" {
+  value       = aws_s3_bucket_website_configuration.frontend_config.website_endpoint # Website URL
+  description = "The endpoint of the S3 static website"
 }
